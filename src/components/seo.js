98fd36b24b288478cd39d53d1b651012aadab1a3
Ignat Skoryh <ignat.skoryh@gmail.com>
2021-08-28 09:34:08 +0300

    Simplify Helmet meta tags

--- src/components/seo.js
+++ src/components/seo.js
@@ -17,54 +17,23 @@ function Seo({ description, lang, meta, keywords, title }) {
   `);
 
   const metaDescription = description || site.siteMetadata.description;
+  const metaTitle = `${title} | ${site.siteMetadata.title}`;
 
   return (
     <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: site.siteMetadata.title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:title`,
-          content: site.siteMetadata.title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
-            : []
-        )
-        .concat(meta)}
+      htmlAttributes={{ lang }}
+      meta={meta}
       title={title}
       titleTemplate={`${site.siteMetadata.title} - %s`}
     >
+        <meta name="description" content={metaDescription} />
+        <meta name="og:title" content={metaTitle} />
+        <meta name="og:description" content={metaDescription} />
+        <meta name="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={metaTitle} />
+        <meta name="twitter:description" content={metaDescription} />
+        {keywords.length > 0 && <meta name="keywords" content={keywords.join(', ')} /> }
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
